7e1c27f4ca9bf56c11e045b820b821fc2e39440e
SunwoongShin <dev.ios.edward@gmail.com>
2021-08-01 13:51:18 +0900

    Update All

--- app.js
+++ app.js
@@ -13,3 +13,42 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
 module.exports = app;
+
+// register endpoint
+app.post("/register", (request, response) => {
+    // hash the password
+    bcrypt
+      .hash(request.body.password, 10)
+      .then((hashedPassword) => {
+        // create a new user instance and collect the data
+        const user = new User({
+          email: request.body.email,
+          password: hashedPassword,
+        });
+  
+        // save the new user
+        user
+          .save()
+          // return success if the new user is added to the database successfully
+          .then((result) => {
+            response.status(201).send({
+              message: "User Created Successfully",
+              result,
+            });
+          })
+          // catch erroe if the new user wasn't added successfully to the database
+          .catch((error) => {
+            response.status(500).send({
+              message: "Error creating user",
+              error,
+            });
+          });
+      })
+      // catch error if the password hash isn't successful
+      .catch((e) => {
+        response.status(500).send({
+          message: "Password was not hashed successfully",
+          e,
+        });
+      });
+  });
